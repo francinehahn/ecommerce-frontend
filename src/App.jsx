97ae1322262886cc5ec202192e7fdaabdc3ca68d
Francine Hahn <fran_hahn@hotmail.com>
2023-03-28 22:52:06 -0300

    feat: I added a useRequestData hook in the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import { Router } from './routes/Router'
 import { GlobalStyle } from './globalStyle'
+import { useRequestData } from './hooks/useRequestData'
 
 
 function App() {
-
-  function closingCode() {
-    localStorage.removeItem("token")
-    return null
+  const token = localStorage.getItem("token")
+  
+  if (token) {
+    const [isLoadingUserData, userData, userError] = useRequestData("https://ecommerce-backend-8st9.onrender.com/users/profile", token)
+    if (!isLoadingUserData && !userData && userError) {
+      localStorage.removeItem("token")
+    }
   }
-
-  window.onbeforeunload = closingCode()
 
   return (
     <>
