9cbfcc094390cfe7fa4bb6f95c60a0d9a498ef91
Francine Hahn <fran_hahn@hotmail.com>
2023-05-06 23:53:04 -0300

    feat: I improved the responsiveness of the product in cart component

--- src/components/ProductInCart/style.jsx
+++ src/components/ProductInCart/style.jsx
@@ -6,51 +6,28 @@ export const Container = styled.div `
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
-    padding: 0 5vw;
+    padding: .5rem 2rem;
     border-bottom: 1px solid grey;
-    margin-bottom: 5vh;
-
-    @media screen and (min-width: 750px) {
-        gap: 5vw;
-        padding: 0 5vw;
+    margin: 0 5rem 2rem 5rem;
+    gap: 2rem;
+    
+    @media screen and (max-width: 750px) {
+        gap: 1rem;
+        margin: 0 2rem 2rem 2rem;
     }
-    @media screen and (min-width: 630px) and (max-width: 750px) {
-        gap: 5vw;
-        padding: 0 4vw;
-    }
-    @media screen and (max-width: 630px) {
-        gap: 3vw;
-        padding: 0 4vw;
+    @media screen and (max-width: 450px) {
+        gap: 1rem;
+        margin: 0 1rem 2rem 1rem;
     }
 
     img {
-        margin-bottom: 1vh;
         border-radius: 1px;
-        @media screen and (min-width: 1200px) {
-            width: 5%;
-        }
-        @media screen and (min-width: 850px) and (max-width: 1200px) {
-            width: 7%;
-        }
-        @media screen and (min-width: 680px) and (max-width: 850px) {
-            width: 10%;
-        }
-        @media screen and (min-width: 500px) and (max-width: 680px) {
-            width: 15%;
-        }
-        @media screen and (max-width: 500px) {
-            width: 20%;
-        }
-    }
-
-    p {
-        margin-bottom: 1vh;
+        width: 5rem;
     }
 
     svg {
-        width: 25px;
-        height: 25px;
-        margin-bottom: 1vh;
+        width: 1.4rem;
+        height: 1.4rem;
         cursor: pointer;
         :hover {
             opacity: .5;
@@ -61,25 +38,17 @@ export const Container = styled.div `
 export const Units = styled.div `
     display: flex;
     border: 0.5px solid grey;
-    margin-bottom: 1vh;
-    
-    @media screen and (min-width: 1000px) {
-        height: 5vh;
-    }
-    @media screen and (max-width: 1000px) {
-        height: 4vh;
-    }
+    height: 2.2rem;
 
     button {
-        font-size: 18px;
-        padding: 0 13px;
+        font-size: 1.1rem;
+        padding: 0 .7rem;
         border: none;
         cursor: pointer;
     }
 
     p {
-        font-size: 16px;
-        padding: 7px 11px;
-        margin-bottom: 1vh;
+        font-size: 1rem;
+        padding: .5rem;
     }
 `
